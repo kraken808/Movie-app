cfb7e2581ea5307e10cdb9da58dcf2ec9e0f62ab
kraken808 <murat99.04@gmail.com>
2021-02-04 09:55:50 +0600

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Murat Merekov on 04.02.2021.
+//  Copyright © 2021 Murat Merekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
